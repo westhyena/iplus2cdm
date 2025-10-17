31a1fc43a0f0bd8223091363f1aa581ee18168b0
Jungwoo (sector) <westhyena@gmail.com>
2025-10-17 15:54:35 +0900

    feat(reset): add measruement & observation period

--- etl-sql/stg/reset.sql
+++ etl-sql/stg/reset.sql
@@ -3,7 +3,9 @@ BEGIN TRY
   BEGIN TRAN;
   DELETE FROM [$(CdmSchema)].[condition_occurrence];
   DELETE FROM [$(CdmSchema)].[drug_exposure];
+  DELETE FROM [$(CdmSchema)].[measurement];
   DELETE FROM [$(CdmSchema)].[visit_occurrence];
+  DELETE FROM [$(CdmSchema)].[observation_period];
   DELETE FROM [$(CdmSchema)].[person];
   COMMIT;
 END TRY
