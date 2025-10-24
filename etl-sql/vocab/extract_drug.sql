5e0892c72a0e9928df9730c58e982270952de864
Jungwoo (sector) <westhyena@gmail.com>
2025-10-24 14:35:04 +0900

    feat(drug): extract drug sql

---
+++ etl-sql/vocab/extract_drug.sql
@@ -0,0 +1,18 @@
+﻿WITH 
+    last_date AS (
+        SELECT
+            청구코드 AS code,
+            max(적용일자) AS apply_date
+        FROM
+            [$(SrcSchema)].[PICMECHM]
+        WHERE
+            수가분류 = 3
+        GROUP BY
+            청구코드
+    )
+SELECT
+    p.*
+FROM
+    [$(SrcSchema)].[PICMECHM] p
+    JOIN last_date d ON p.적용일자 = d.apply_date
+    AND p.청구코드 = d.code
