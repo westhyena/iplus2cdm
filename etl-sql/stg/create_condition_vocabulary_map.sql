fa2122b66c3d60763e3a1f5a6360af2332c2fcba
Jungwoo (sector) <westhyena@gmail.com>
2025-10-24 11:58:13 +0900

    chore(condition): remove conditoin stage map create sql

--- etl-sql/stg/create_condition_vocabulary_map.sql
+++ etl-sql/stg/create_condition_vocabulary_map.sql
@@ -10,13 +10,4 @@ CREATE TABLE [$(StagingSchema)].condition_vocabulary_map (
   concept_id int
 );
 
--- 스테이징 적재용 테이블 (CSV 원본 문자형으로 수용)
-IF OBJECT_ID('$(StagingSchema).condition_vocabulary_map_stage','U') IS NOT NULL
-  DROP TABLE [$(StagingSchema)].[condition_vocabulary_map_stage];
-
-CREATE TABLE [$(StagingSchema)].condition_vocabulary_map_stage (
-  source_code nvarchar(4000) NULL,
-  kor_name nvarchar(4000) NULL,
-  eng_name nvarchar(4000) NULL,
-  concept_id nvarchar(100) NULL
-);
+-- 스테이징 테이블은 동적 생성 (scripts/modules/condition-map.ps1)
