83c56543cbd1b4825068cebfa34c3c1d7bef469f
Jungwoo (sector) <westhyena@gmail.com>
2025-10-31 14:35:58 +0900

    chore(drug): change col name

--- etl-sql/stg/create_drug_vocabulary_map.sql
+++ etl-sql/stg/create_drug_vocabulary_map.sql
@@ -5,7 +5,7 @@ IF OBJECT_ID('$(StagingSchema).drug_vocabulary_map','U') IS NOT NULL
 
 CREATE TABLE [$(StagingSchema)].drug_vocabulary_map (
   source_code nvarchar(200),
-  source_Name nvarchar(500),
+  source_name nvarchar(500),
   target_concept_id int,
   source_concept_id int
 );
