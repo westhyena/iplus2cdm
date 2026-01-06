5305a5522ca5a476d093825b51008bcb782995b1
Jungwoo (sector) <westhyena@gmail.com>
2026-01-06 16:31:27 +0900

    feat: add mssql tools

---
+++ bin/install_mssql_tools.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo curl -sSL -O https://packages.microsoft.com/config/ubuntu/24.04/packages-microsoft-prod.deb
+
+sudo dpkg -i packages-microsoft-prod.deb
+
+sudo apt-get update
+sudo apt-get install mssql-tools18 unixodbc-dev
+
+echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bash_profile
+source ~/.bash_profile
