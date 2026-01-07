e7aa2894e202ea62d8fe6ff2bc8db87a3614f414
Jungwoo (sector) <westhyena@gmail.com>
2026-01-07 17:24:00 +0900

    feat: webapi init add username & pw

--- config/insert_source.sql
+++ config/insert_source.sql
@@ -1,16 +1,23 @@
+TRUNCATE ohdsi.source;
+TRUNCATE ohdsi.source_daimon;
+
 INSERT INTO ohdsi.source (
     source_id,           -- 유니크한 ID (자동 증가가 아니면 수동 기입)
     source_name,         -- ATLAS 화면에 표시될 이름 (예: '정우님 로컬 데이터')
     source_key,          -- 시스템 내부에서 식별자로 쓸 고유 키 (영문/숫자/언더바)
     source_connection,   -- 실제 CDM DB의 JDBC 연결 문자열
-    source_dialect       -- DB 종류 (postgresql, sql server, oracle 등)
+    source_dialect,      -- DB 종류 (postgresql, sql server, oracle 등)
+    username,
+    password
 )
 VALUES (
     1, 
     '$CDM_SOURCE_NAME',
     '$CDM_SOURCE_ABBREVIATION',
     'jdbc:postgresql://$POSTGRES_SERVER:$POSTGRES_PORT/$POSTGRES_DB',
-    'postgresql'
+    'postgresql',
+    '$POSTGRES_USER',
+    '$POSTGRES_PASSWORD'
 );
 
 INSERT INTO ohdsi.source_daimon (source_id, daimon_type, table_qualifier, priority) 
