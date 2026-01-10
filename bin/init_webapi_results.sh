86d15c5cb8287fd75253a90c6d5b9fa62b4157e9
Jungwoo (sector) <westhyena@gmail.com>
2026-01-10 10:34:22 +0900

    feat: add achilless schema init in webapiresult

--- bin/init_webapi_results.sh
+++ bin/init_webapi_results.sh
@@ -3,6 +3,7 @@ if [ -f .env ]; then
   export $(grep -v '^#' .env | xargs)
 fi
 
+echo =========== Results ===========
 # 1. WebAPI로부터 SQL 받아오기
 # dialect: postgresql, schema: 결과스키마, vocabSchema: 보카스키마
 SQL_URL="http://localhost:8080/WebAPI/ddl/results?dialect=postgresql&schema=$OMOP_ACHILLES_RESULTS_SCHEMA&vocabSchema=$OMOP_CDM_SCHEMA&initConceptHierarchy=true"
@@ -16,5 +17,19 @@ echo "Executing SQL on PostgreSQL..."
 PGPASSWORD=$POSTGRES_PASSWORD psql -h localhost -U $POSTGRES_USER -d $POSTGRES_DB -f init_hierarchy.sql
 
 rm init_hierarchy.sql
+
+
+echo =========== Achilles ===========
+SQL_URL = http://localhost:8080/WebAPI/ddl/achilles?dialect=postgresql&schema=$OMOP_ACHILLES_RESULTS_SCHEMA&vocabSchema=$OMOP_CDM_SCHEMA
+
+echo "Fetching DDL from WebAPI..."
+curl -s $SQL_URL > init_concept_count.sql
+
+# 2. psql을 이용해 바로 실행
+echo "Executing SQL on PostgreSQL..."
+
+PGPASSWORD=$POSTGRES_PASSWORD psql -h localhost -U $POSTGRES_USER -d $POSTGRES_DB -f init_concept_count.sql
+
+rm init_concept_count.sql
 
 echo "Done! concept_hierarchy table is now created and populated."
